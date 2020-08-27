817f1fbdb0332f0bf494531398108c9e65bd927e
jamesjianpeng <jamesjianpeng@gmail.com>
2020-08-28 02:04:47 +0800

    perf: 添加注册文件

---
+++ play/src/pages/Register/Register.vue
@@ -0,0 +1,63 @@
+<template>
+  <q-page class="bg-light-green window-height window-width row justify-center items-center">
+    <div class="column shadow-8">
+      <div class="row justify-center items-center">
+        <h5 class="text-h5 text-white q-my-md text-weight-bolder">LIVE & LEARN</h5>
+      </div>
+      <div class="row">
+        <q-card square bordered class="q-pa-lg shadow-1">
+          <q-card-section>
+            <q-form class="q-gutter-md">
+              <q-input square filled clearable v-model="email" type="email" label="email" />
+              <q-input square filled clearable v-model="password" type="password" label="password" />
+            </q-form>
+          </q-card-section>
+          <q-card-actions class="q-px-md">
+            <q-btn unelevated color="light-green-7" @click="login" size="lg" class="full-width" label="Login" />
+          </q-card-actions>
+          <q-card-section class="text-center q-pa-none">
+            <p class="text-grey-6">Not reigistered? Created an Account </p>
+          </q-card-section>
+          <q-card-section class="text-center q-pa-none">
+            <p class="text-grey-6">{{gettersTest}}</p>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { mapActions, mapGetters, mapState } from 'vuex'
+export default {
+  name: 'Login',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  computed: {
+    ...mapGetters('login', ['gettersTest']),
+    ...mapState('login', ['stateTest'])
+  },
+  methods: {
+    ...mapActions([
+      'login/actionsTest',
+      'login/testApi'
+    ]),
+    login () {
+      console.log(this.stateTest)
+      const gettersTest = this.gettersTest ? this.gettersTest + '1' : ''
+      this['login/actionsTest']({ test: `hi ${gettersTest}` })
+      this['login/testApi']({ test: `hi ${gettersTest}` })
+    }
+  }
+}
+</script>
+
+<style>
+.q-card {
+  width: 360px;
+}
+</style>
