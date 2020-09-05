77194461a2f2bd1750a8cbbd32598553af1a295c
jamesjianpeng <jamesjianpeng@gmail.com>
2020-09-05 09:49:44 +0800

    perf: 辅助函数 dbUrlToObject

---
+++ live-learn-server/src/core/utils.ts
@@ -0,0 +1,24 @@
+/**
+ * @description mongo / redis 的 url 转为 host/port/db/user/password 对象
+ * @param url redis://<user>:<password>@<host>:<port>/<db>
+ * @param prefix <redis://>或者<mongodb://>这个前缀过滤掉
+ */
+export const dbUrlToObject = (url: string, prefix?: string) => {
+  if (!url) {
+      console.error(`${url} `)
+      return {}
+  }
+  prefix = prefix || 'redis://'
+  url = url.replace(prefix, '')
+  const user =  url.split(':')[0]
+  const passwordAndHost = url.split(':')[1]
+  const portAndDb = url.split(':')[2]
+  const res = {
+      host: passwordAndHost.split('@')[1],
+      port: portAndDb.split('/')[0],
+      db: portAndDb.split('/')[1], // mongodb 的 db 是 undefined
+      user,
+      password: passwordAndHost.split('@')[0],
+  }
+  return res
+}
--- live-learn-server/src/module/upload/upload.module.ts
+++ live-learn-server/src/module/upload/upload.module.ts
@@ -4,15 +4,14 @@ import { UploadService } from './upload.service';
 import { UploadProcessor } from './upload.processor';
 import { BullModule } from '@nestjs/bull';
 import { QUEUE_NAME_UPLOAD } from './constants';
+import settings from '../../../settings.json'
+import { dbUrlToObject } from 'src/core/utils'
+
 @Module({
   imports: [
     BullModule.registerQueue({
       name: QUEUE_NAME_UPLOAD, // 队列的名字
-      redis: { // redis 这个配置不传，则默认是 localhost:6379/0
-        host: 'localhost',
-        port: 6379,
-        db: 2, // db 默认是 0
-      },
+      redis: dbUrlToObject(settings.redis),
     }),
   ],
   controllers: [UploadController],
