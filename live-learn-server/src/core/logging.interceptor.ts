8307d85fb7993a1d1736b8fef65f06a39f6c7b75
jamesjianpeng <jamesjianpeng@gmail.com>
2020-09-06 10:10:07 +0800

    perf: logger  接入nestjs logger

--- live-learn-server/src/core/logging.interceptor.ts
+++ live-learn-server/src/core/logging.interceptor.ts
@@ -3,43 +3,30 @@ import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import winston, { format } from 'winston';
 import { NestjsWinstonLoggingLibService } from './nestjs-winston-logging-lib';
-
-const serviceName = 'service';
-
-const logger = winston.createLogger({
-  level: 'info',
-  format: format.combine(
-    format.label({ label: '[my-label]' }),
-    format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss',
-    }),
-    format.simple(),
-  ),
-  defaultMeta: { service: serviceName },
-  transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'warn.log', level: 'warn' }),
-    new winston.transports.File({ filename: 'info.log' }),
-  ],
-});
+import { NestjsLoggerLibService } from './nestjs-logger-lib'
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
   constructor(
      private nestjsWinstonLoggingLibService: NestjsWinstonLoggingLibService,
+     private nestjsLoggerLibService: NestjsLoggerLibService,
   ) {
-
   }
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    console.log('Before...');
+    const args = context.getArgs()
+    const [ req ] = args
     this.nestjsWinstonLoggingLibService.log({ level: 'info', text: 'Before...' });
+    this.nestjsLoggerLibService.log(`request {${req.url}, ${req.method}} `, 'loggingIntercepter')
 
     const now = Date.now();
     return next
       .handle()
       .pipe(
-        tap(() => console.log(`After... ${Date.now() - now}ms`)),
+        tap(() => {
+          const diff = Date.now() - now
+          this.nestjsLoggerLibService.log(`request {${req.url}, ${req.method}} spend time: ${diff}ms`, 'loggingIntercepter')
+        }),
       );
   }
 }
