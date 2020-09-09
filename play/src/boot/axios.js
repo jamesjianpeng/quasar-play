b774505bdcfaf23e1d98f5614d9e690db05e79cd
jamesjianpeng <jamesjianpeng@gmail.com>
2020-09-10 00:11:58 +0800

    perf: quasar axios response 拦截

--- play/src/boot/axios.js
+++ play/src/boot/axios.js
@@ -1,4 +1,12 @@
 import Vue from 'vue'
 import axios from 'axios'
+axios.interceptors.response.use(response => {
+  // 对响应数据做些什么
+  return response.data
+}, err => {
+  // 对响应错误做些什么
+  console.log('err', err)
+  return Promise.reject(err)
+})
 
 Vue.prototype.$axios = axios
